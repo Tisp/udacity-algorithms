fce0d9acbe732a60e47f7ebf0b774ca83e83fe56
Thiago Pereira <thiago.pereira@evino.com.br>
2020-06-03 22:48:29 -0300

    creating queues

---
+++ queue/test_queue.py
@@ -0,0 +1,18 @@
+import pytest
+from queue.queue import Queue
+
+
+class TestQueue:
+
+    def test_queue(self):
+        q = Queue(1)
+        q.enqueue(2)
+        q.enqueue(3)
+        assert q.peek() == 1
+        assert q.dequeue() == 1
+        q.enqueue(4)
+        assert q.dequeue() == 2
+        assert  q.dequeue() == 3
+        assert q.dequeue() == 4
+        q.enqueue(5)
+        assert q.peek() == 5
